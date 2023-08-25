50cfdc0eb5e300d676c1f73a016b66477bcdac8a
SyberiaK <syberiakey@gmail.com>
2023-08-25 16:50:57 +0500

    Tests: use @pytest.mark.parametrize instead of copying tests

--- tests/test_parser_impl.py
+++ tests/test_parser_impl.py
@@ -1,5 +1,7 @@
+import json
 from pathlib import Path
 
+import pytest
 import rapidjson
 import simplejson
 from sl10n import SL10n
@@ -9,39 +11,12 @@ import ujson
 from . import *
 
 
-def test_parser_impl_simplejson():
+@pytest.mark.parametrize("module", [
+    json, simplejson, rapidjson, ujson
+])
+def test_parsing_impl_basic(module):
     path = Path(__file__).parent / 'data' / 'test_locale_en'
-    l10n = SL10n(Locale, path, parsing_impl=JSONImpl(simplejson)).init()
-
-    is_equal(l10n.default_lang, EN)
-
-    locale: Locale = l10n.locale()
-    is_equal(type(locale), Locale)
-
-    is_equal(locale.lang_code, EN)
-    is_equal(locale.topic_title, "Basic 'for' loop algorithm")
-    is_equal(locale.topic_text, TOPIC_TEXT_EN)
-    is_equal(locale.topic_conclusion, "Now you know basic 'for' loop algorithm!")
-
-
-def test_parser_impl_rapidjson():
-    path = Path(__file__).parent / 'data' / 'test_locale_en'
-    l10n = SL10n(Locale, path, parsing_impl=JSONImpl(rapidjson)).init()
-
-    is_equal(l10n.default_lang, EN)
-
-    locale: Locale = l10n.locale()
-    is_equal(type(locale), Locale)
-
-    is_equal(locale.lang_code, EN)
-    is_equal(locale.topic_title, "Basic 'for' loop algorithm")
-    is_equal(locale.topic_text, TOPIC_TEXT_EN)
-    is_equal(locale.topic_conclusion, "Now you know basic 'for' loop algorithm!")
-
-
-def test_parser_impl_ujson():
-    path = Path(__file__).parent / 'data' / 'test_locale_en'
-    l10n = SL10n(Locale, path, parsing_impl=JSONImpl(ujson)).init()
+    l10n = SL10n(Locale, path, parsing_impl=JSONImpl(module)).init()
 
     is_equal(l10n.default_lang, EN)
 
