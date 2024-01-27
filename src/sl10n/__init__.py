e225400f0e0f29e03f0c0cc5db5ec263aac4210c
SyberiaK <syberiakey@gmail.com>
2024-01-27 19:10:14 +0500

    Strict mode (№1) and detecting unfilled keys; debug logs instead of info logs

--- src/sl10n/__init__.py
+++ src/sl10n/__init__.py
@@ -1,11 +1,9 @@
 """Static localization system that reduces the headache of working with localization"""
 
-__version__ = '0.3.0.0'
-
-import logging
-
 UTF8 = 'utf-8'
-LOGGER = logging.getLogger('sl10n')
 
 from .core import SL10n
 from .locale import SLocale
+
+__all__ = ['SL10n', 'SLocale']
+__version__ = '0.3.0.0'
--- src/sl10n/process.py
+++ src/sl10n/process.py
@@ -1,40 +1,44 @@
+"""
+HEY, STOP RIGHT THERE!
+
+Be aware that this code is not intended to be used outside the module.
+Any implementation detail can be changed at any time without warning.
+If you need to manipulate it in any way, you're absolutely screwed.
+"""
+
 from __future__ import annotations
 
 from dataclasses import fields
+import logging
 from pathlib import Path
-from typing import Any, Type, TypeVar
+from typing import Type, TypeVar
 import warnings
 
-from . import LOGGER, UTF8
+from . import UTF8
 from .pimpl import ParsingImpl
 from .locale import SLocale
 from .modifiers import PreModifiers, PostModifiers
-from .warnings import UndefinedLocaleKey, UnexpectedLocaleKey, UnfilledLocaleKey
+from .warnings import UndefinedLocaleKey, UnexpectedLocaleKey, UnfilledLocaleKey, UnknownModifier
 
 T = TypeVar('T')
+logger = logging.getLogger('sl10n')
 
 
 class _LocaleProcessor:
-    """
-    Class that processes locales. Calling this returns a locale container.
-
-    This class is not intended to be inherited.
-    """
-
     EXCLUDE_SIGNAL = 0x01
 
     filepath: Path
-    parsing_impl: ParsingImpl
-    data: dict[str, Any]
-    lc_fields: list[str]
+    data: dict
     all_dumped_fields: list[str]
     
-    def __init__(self, locale_container: Type[T], parsing_impl: ParsingImpl):
+    def __init__(self, locale_container: Type[T], parsing_impl: ParsingImpl, is_strict: bool, warn_unfilled_keys: bool):
         self.locale_container = locale_container
         self.parsing_impl = parsing_impl
+        self.is_strict = is_strict
+        self.warn_unfilled_keys = warn_unfilled_keys
 
         self.all_fields = [k.name for k in fields(locale_container)]
-        self.lc_fields = [k.name for k in fields(locale_container) if k not in fields(SLocale)]
+        self.lc_fields = {k.name: k.type for k in fields(locale_container) if k not in fields(SLocale)}
         
     def process(self, filepath: Path) -> T | None:
         self.filepath = filepath
@@ -51,12 +55,16 @@ class _LocaleProcessor:
             return
 
         unexpected_keys = self.find_unexpected_keys(self.all_fields + used_modifiers)
-        
-        self.all_dumped_fields = self.lc_fields + used_modifiers + unexpected_keys
+        undefined_keys = self.find_undefined_keys()
+        if self.warn_unfilled_keys:
+            self.check_unfilled_keys()
 
-        self.check_unfilled_keys()
+        self.all_dumped_fields = list(self.lc_fields.keys()) + used_modifiers + unexpected_keys
 
-        if self.any_undefined_key() or unexpected_keys:
+        for key in undefined_keys:
+            self.data[key] = key
+
+        if undefined_keys or unexpected_keys:
             self.redump()
 
         self.apply_postmodifiers(postmodifiers)
@@ -84,15 +92,15 @@ class _LocaleProcessor:
 
     def apply_premodifiers(self, premodifiers: PreModifiers):
         if premodifiers.exclude:
-            LOGGER.info(f'Excluding {self.filepath.name}...')
+            logger.debug(f'Excluding {self.filepath.name}...')
             return self.EXCLUDE_SIGNAL
 
     def apply_postmodifiers(self, postmodifiers: PostModifiers):
         if postmodifiers.redump:
-            LOGGER.info(f'Redumping {self.filepath.name}...')
+            logger.debug(f'Redumping {self.filepath.name}...')
             self.redump()
         if postmodifiers.lang_code:
-            LOGGER.info(f'Changing lang code of "{self.filepath.name}" to "{postmodifiers.lang_code}"')
+            logger.debug(f'Changing lang code of "{self.filepath.name}" to "{postmodifiers.lang_code}"')
             self.data['lang_code'] = postmodifiers.lang_code
         else:
             self.data['lang_code'] = self.filepath.stem
@@ -102,24 +110,28 @@ class _LocaleProcessor:
             self.data = {key: self.data[key] for key in self.all_dumped_fields}  # fixing pairs order
             self.parsing_impl.dump(self.data, f)
 
-    def any_undefined_key(self):
-        undefined_keys = set(self.lc_fields) - set(self.data)
+    def find_undefined_keys(self):
+        undefined_keys = set(self.lc_fields.keys()) - set(self.data)
+
         for key in undefined_keys:
             warnings.warn(f'Found undefined key "{key}" in "{self.filepath}"', UndefinedLocaleKey, stacklevel=4)
-            self.data[key] = key
 
-        return bool(undefined_keys)
+        return list(undefined_keys)
 
     def find_unexpected_keys(self, possible_fields):
         unexpected_keys = set(self.data) - set(possible_fields)
 
         for key in unexpected_keys:
-            warnings.warn(f'Got unexpected key "{key}" in "{self.filepath}"', UnexpectedLocaleKey, stacklevel=4)
+
+            if key.startswith('$'):
+                warnings.warn(f'Found unknown modifier "{key}" in "{self.filepath}"', UnknownModifier, stacklevel=4)
+            else:
+                warnings.warn(f'Found unexpected key "{key}" in "{self.filepath}"', UnexpectedLocaleKey, stacklevel=4)
 
         return list(unexpected_keys)
 
     def check_unfilled_keys(self):
-        unfilled_keys = set(k for k, v in self.data.items() if k == v or k == '')
+        unfilled_keys = set(k for k, v in self.data.items() if k == v or v == '')
 
         for key in unfilled_keys:
             warnings.warn(f'Got unfilled key "{key}" in "{self.filepath}"', UnfilledLocaleKey, stacklevel=4)
